66e37220b0ed3ae74ce16833f092502c2203d339
Dezzz <deziahgayle@gmail.com>
2023-02-16 20:39:27 -0500

    test function with a date string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 // add your code as appropriate
-
-
 #include <iostream>
 
 #include <fstream>
@@ -9,30 +7,14 @@
 
 #include <string>
 
-int main() {
-    std::string date;
-    double eastSt, eastEl, westSt, westEl;
-    // Create an input file stream object and open the file
-    std::ifstream input_file("Current_Reservoir_Levels.tsv");
-    
-    // Check if the file was opened successfully
-    if (!input_file) {
-        std::cerr << "Error opening file" << std::endl;
-        return 1;
-    }
-    
-    // Read the contents of the file
-    std::string line;
-    std::getline(input_file, line);
-    while(input_file >> date >> eastSt >> eastEl >> westSt >> westEl) {
-        // this loop reads the file line-by-line
-        // extracting 5 values on each iteration
-        
-        input_file.ignore(INT_MAX, '\n'); //skips to the end of line,
-                              //ignorring the remaining columns
+#include "reservoir.h"
 
-        // for example, to print the date and East basin storage:
-        std::cout << date << " " << eastSt << std::endl;
+int main() {
+        // Test the get_east_storage function with a date string
+        std::string date = "01/01/2018";
+        double east_storage = get_east_storage(date);
+        std::cout << "East basin storage on " << date << " was " << east_storage << " billion gallons." << std::endl;
+
+        return 0;
     }
-    return 0;
-}
+    
