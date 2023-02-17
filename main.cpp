038efecc71542255b80a60e31bc6657e9f584c13
Dezzz <deziahgayle@gmail.com>
2023-02-17 00:12:21 -0500

    tested get_min_east function and printed result

--- main.cpp
+++ main.cpp
@@ -10,10 +10,13 @@
 #include "reservoir.h"
 
 int main() {
-        // Test the get_east_storage function with a date string
-        std::string date = "01/01/2018";
-        double east_storage = get_east_storage(date);
-        std::cout << "East basin storage on " << date << " was " << east_storage << " billion gallons." << std::endl;
+    // Test the get_east_storage function with a date string
+    std::string date = "01/01/2018";
+    double east_storage = get_east_storage(date);
+    std::cout << "East basin storage on " << date << " was " << east_storage << " billion gallons." << std::endl;
+    
+    double lowest_storage = get_min_east();
+    std::cout << "The minimum storage in the East Basin in 2018 is "<< lowest_storage << std::endl;
 
         return 0;
     }
